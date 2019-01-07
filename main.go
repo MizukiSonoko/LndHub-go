9d83e024ec9d5cabe721c8cddc31238d45ac4a0e
MizukiSonoko <sonoko@mizuki.io>
2019-01-07 21:01:06 +0900

    Add Lnd interface and impl class

--- main.go
+++ main.go
@@ -10,7 +10,36 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
+// ToDo: make constructor func and hide member because we should not change it.
+type IndInfo struct {
+	LightningId     string
+	IdentityAddress string
+	IdentityPubkey  string
+}
+
+type Lnd interface {
+	GetInfo() (IndInfo, error)
+}
+
+type lndClient struct {
+	client lnrpc.LightningClient
+	c      context.Context
+}
+
+func (l *lndClient) GetInfo() (IndInfo, error) {
+	res, err := l.client.GetInfo(l.c, &lnrpc.GetInfoRequest{})
+	if err != nil {
+		return IndInfo{}, fmt.Errorf("grpc error err:%s", err)
+	}
+	return IndInfo{
+		res.LightningId,
+		res.IdentityAddress,
+		res.IdentityPubkey,
+	}, nil
+}
+
 func getClient(address, tlsPath string) *grpc.ClientConn {
+	_ = os.Setenv("GRPC_SSL_CIPHER_SUITES", "HIGH+ECDSA")
 
 	creds, err := credentials.NewClientTLSFromFile(tlsPath, "")
 	if err != nil {
@@ -31,16 +60,24 @@ func getClient(address, tlsPath string) *grpc.ClientConn {
 	return conn
 }
 
-func main() {
-	_ = os.Setenv("GRPC_SSL_CIPHER_SUITES", "HIGH+ECDSA")
-
-	// I use macOS
-	client := lnrpc.NewLightningClient(getClient(
-		"localhost:10009", os.Getenv("HOME")+"/Library/Application Support/Lnd/tls.cert"))
-	res, err := client.GetInfo(context.Background(), &lnrpc.GetInfoRequest{})
-	if err != nil {
-		fmt.Printf("GetInfo failed err:%s", err)
-		os.Exit(1)
+func newLnd(address, certPath string) Lnd {
+	return &lndClient{
+		lnrpc.NewLightningClient(getClient(address, certPath)),
+		context.Background(),
 	}
-	fmt.Printf("lightningId:%s\n", res.LightningId)
+}
+
+func main() {
+	// I use macOS
+	lnd := newLnd("localhost:10009", os.Getenv("HOME")+"/Library/Application Support/Lnd/tls.cert")
+	{
+		res, err := lnd.GetInfo()
+		if err != nil {
+			fmt.Printf("GetInfo failed err:%s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("lightningId:%s\n", res.LightningId)
+		fmt.Printf("IdentityAddress:%s\n", res.IdentityAddress)
+		fmt.Printf("IdentityPubkey:%s\n", res.IdentityPubkey)
+	}
 }
