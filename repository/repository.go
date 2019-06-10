19ca031bb0c03ff350b6a461cf30b4d965508fd9
MizukiSonoko <sonoko@mizuki.io>
2019-06-10 02:29:01 +0000

    Add level db impl to repo

--- repository/repository.go
+++ repository/repository.go
@@ -1,6 +1,19 @@
 package repository
 
-import e "github.com/MizukiSonoko/LndHub-go/entity"
+import (
+	e "github.com/MizukiSonoko/LndHub-go/entity"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+var db *leveldb.DB
+
+func init() {
+	d, err := leveldb.OpenFile("~/.lndhub-go/db", nil)
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
 
 type UserRepo interface {
 	Get(id string) e.User
@@ -9,10 +22,19 @@ type UserRepo interface {
 }
 
 type userRepo struct {
+	db *leveldb.DB
 }
 
-func (repo *userRepo) Get(id string) e.User {
-	return e.User{}
+func (repo *userRepo) GetBitcoinAddress(id string) (string, error) {
+	address, err := repo.db.Get([]byte("bitcoin_address_for_"+id), nil)
+	if err != nil {
+		return "", err
+	}
+	return address, nil
+}
+
+func (repo *userRepo) SetBitcoinAddress(id, address string) error {
+	return db.Put([]byte("bitcoin_address_for_"+id), []byte("address"), nil)
 }
 
 func (repo *userRepo) Update(e.User) error {
